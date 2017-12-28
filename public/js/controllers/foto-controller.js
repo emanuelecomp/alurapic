e4ed439027687d4e92d3e6e6f0733f31050e95d8
Emanuel Lucas de Sousa e Silva <cardgameheritage@gmail.com>
2017-12-27 22:36:28 -0300

    funcoes de remover e editar foram adicionadas

---
+++ public/js/controllers/foto-controller.js
@@ -0,0 +1,43 @@
+angular.module('alurapic').controller('FotoController',function($scope,$http,$routeParams){
+    $scope.foto = {};
+    $scope.mensagem = "";
+
+    if($routeParams.fotoId){
+        $http.get('v1/fotos/' + $routeParams.fotoId)
+        .success(function(foto){
+            $scope.foto = foto;
+        })
+        .error(function(erro){
+            console.log(erro);
+            $scope.mensagem = "Não foi possível obter a foto.";
+        });
+    }
+
+    $scope.submeter = function(){
+        if($scope.formulario.$valid){
+            if($scope.foto._id){
+                $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+                .success(function(){
+                    $scope.mensagem = "Foto editada com sucesso";
+                })
+                .error(function(erro){
+                    console.log(erro);
+                    $scope.mensagem = "Foto não editada";
+                });
+            }else{
+                $http.post('v1/fotos',$scope.foto)
+                .success(function(){
+                    $scope.foto = {};
+                    $scope.mensagem = "Foto cadastrada com sucesso.";
+                    
+                })
+                .error(function(erro){
+                    $scope.mensagem = "Erro. Foto não incluída."
+                });
+            }
+            
+        }
+        
+    };
+
+});
--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,12 @@ angular.module('alurapic',['minhasDiretivas','ngAnimate','ngRoute'])
     
     $routeProvider.when('/fotos/new',{
         templateUrl: 'partials/foto.html',
-        controller: 'FotosController'
+        controller: 'FotoController'
+    });
+
+    $routeProvider.when('/fotos/edit/:fotoId',{
+        templateUrl: 'partials/foto.html',
+        controller: 'FotoController'
     });
    
     $routeProvider.otherwise({ redirectTo: '/fotos'});
